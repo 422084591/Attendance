4447c7dfa8f802d269c0d759cf9a359a4c556fec
422084591 <49331439+422084591@users.noreply.github.com>
2019-07-07 14:47:56 +0800

    1

--- Attendance/src/service/AttendanceService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.Admin;
-
-public interface AttendanceService {
-	public Admin getAdminByUP(String username,String password);
-	public boolean loginByUP(String username,String password);
-	public boolean isExit(String username);
-}
--- Attendance/src/util/SessionManger.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpSession;
-public class SessionManger {
-	public static Map<String,HttpSession> sessionMap=new HashMap<String,HttpSession>();
-}
