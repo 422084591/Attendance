fe08d670f1acfacdc9867d7fdadfc548accfd7c1
422084591 <49331439+422084591@users.noreply.github.com>
2019-07-08 20:51:28 +0800

    1.1

--- Attendance/src/model/CheckReportPageCount.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class CheckReportPageCount {
-	private CheckReportPageCount() {};
-	private static final CheckReportPageCount checkReportPageCount=new CheckReportPageCount();	//单例模式
-	public static CheckReportPageCount getInstance() {
-		return checkReportPageCount;
-	}
-}
